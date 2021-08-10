2942bafaf467e94193f9b27a58d699fd76332787
mwsleonardo <mwsleonardo@gmail.com>
2021-08-10 20:55:24 -0300

    Initial commit

---
+++ src/components/homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Homepage() {
+
+    
+
+
+return (
+    <div>Hello World!!!</div>    
+    
+    )
+}
+
+
+export default Homepage;
---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Homepage from './components/homepage';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+       <Homepage />
+  </React.StrictMode>,
+  document.getElementById('root')
+
+);
