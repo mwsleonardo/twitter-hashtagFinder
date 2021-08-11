e6054bd7258113cb1dfcfe3a22264a7eb1956704
flavioxe <flaviomhenrique@mgail.com>
2021-08-11 13:24:08 -0300

    Início do HTML do topo e do rodapé

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -7,9 +7,25 @@ function Homepage() {
 
 
 return (
-    <div><div>Hello World!!!</div>
-        <div>teste01</div>
-    </div>    
+    <div>
+            <header className="banner">
+                <div className="title">
+                    <img src={LogoWhite} alt="Logo" class="logo"></img>
+                    <div className="buttons">
+                        <button className="aboutButton"> Sobre </button>
+                        <button className="loginButton"> Login </button>
+                    </div>
+                </div>
+                <div className="textBox">
+                    <h1 className="title"> Encontre hashtags de maneira fácil  </h1>
+                    <p className="subtitle"> Digite o que deseja no campo de buscas e confira os resultados do Twitter abaixo </p>
+                </div>
+            </header>
+            <footer>
+                <p className="footerText"> @Newtab Academy 2021. Todos os direitos reservados. </p>
+            </footer>
+        </div>
+    
     
     )
 }
