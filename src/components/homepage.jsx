13c1be841f8070aec1aafbe489028b539314d458
mwsleonardo <mwsleonardo@gmail.com>
2021-08-11 19:31:26 -0300

    Import das imagens e homepage.css
    Realizado os imports para a homepage, e atualizado o esqueleto do html

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-
+import './homepage.css'
+import homepageFooter from "./imgs/homepageFooter.png"
+import logoWhite from "./imgs/logo-white.svg"
 
 function Homepage() {
 
-    
-
-
 return (
     <div>
+        <div className="homepage">
+            
             <header className="banner">
                 <div className="title">
-                    <img src={LogoWhite} alt="Logo" class="logo"></img>
+                    <img src={logoWhite }alt="Logo" class="logo"></img>
                     <div className="buttons">
                         <button className="aboutButton"> Sobre </button>
                         <button className="loginButton"> Login </button>
@@ -20,11 +21,28 @@ return (
                     <h1 className="title"> Encontre hashtags de maneira fácil  </h1>
                     <p className="subtitle"> Digite o que deseja no campo de buscas e confira os resultados do Twitter abaixo </p>
                 </div>
+
+                <input type="text" clasName="searchBar"></input>
+                
             </header>
+
+
+            <div className="container">
+                    <div className="results">
+                        <div className="resultsTitle">
+                            <div className="resultsImgs"></div>
+                            <div className="resultsPosts"></div>
+                        </div>
+                    </div>
+                </div>
+
             <footer>
-                <p className="footerText"> @Newtab Academy 2021. Todos os direitos reservados. </p>
+                <img className="footer" src={homepageFooter}/>
             </footer>
+
         </div>
+
+    </div>
     
     
     )
