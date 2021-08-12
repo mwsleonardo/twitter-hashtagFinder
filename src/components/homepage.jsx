f9d7b8c9d5b206e95b3e2043ec112b8244635d15
flavioxe <flaviomhenrique@mgail.com>
2021-08-11 22:07:27 -0300

    Início do responsivo do header, import do bg mobile e do fontAwesome

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -2,32 +2,36 @@ import React from 'react';
 import './homepage.css'
 import homepageFooter from "./imgs/homepageFooter.png"
 import logoWhite from "./imgs/logo-white.svg"
+// Ícones do FontAwesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { faUserAlt } from "@fortawesome/free-solid-svg-icons";
 
 function Homepage() {
 
-return (
-    <div>
-        <div className="homepage">
-            
-            <header className="banner">
-                <div className="title">
-                    <img src={logoWhite }alt="Logo" class="logo"></img>
-                    <div className="buttons">
-                        <button className="aboutButton"> Sobre </button>
-                        <button className="loginButton"> Login </button>
+    return (
+        <div>
+            <div className="homepage">
+
+                <header className="banner">
+                    <div className="title">
+                        <img src={logoWhite} alt="Logo" class="logo"></img>
+                        <div className="buttons">
+                            <button className="aboutButton"> <FontAwesomeIcon icon={faInfoCircle} /> Sobre </button>
+                            <button className="loginButton"> <FontAwesomeIcon icon={faUserAlt} /> Login </button>
+                        </div>
                     </div>
-                </div>
-                <div className="textBox">
-                    <h1 className="title"> Encontre hashtags de maneira fácil  </h1>
-                    <p className="subtitle"> Digite o que deseja no campo de buscas e confira os resultados do Twitter abaixo </p>
-                </div>
+                    <div className="textBox">
+                        <h1 className="title"> Encontre hashtags de maneira fácil  </h1>
+                        <p className="subtitle"> Digite o que deseja no campo de buscas e confira os resultados do Twitter abaixo </p>
+                    </div>
 
-                <input type="text" clasName="searchBar"></input>
-                
-            </header>
+                    <input type="text" clasName="searchBar"></input>
+
+                </header>
 
 
-            <div className="container">
+                <div className="container">
                     <div className="results">
                         <div className="resultsTitle">
                             <div className="resultsImgs"></div>
@@ -36,15 +40,15 @@ return (
                     </div>
                 </div>
 
-            <footer>
-                <img className="footer" src={homepageFooter}/>
-            </footer>
+                <footer>
+                    <img className="footer" src={homepageFooter} />
+                </footer>
+
+            </div>
 
         </div>
 
-    </div>
-    
-    
+
     )
 }
 
