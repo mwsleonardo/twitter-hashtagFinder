f39b74662ff7a006b1a1f08c7856a2ae1e2fafec
Giovanni Shiroma <giovanni.shiroma04@gmail.com>
2021-08-11 22:09:05 -0300

    inicio do html/css página de login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import "./login.css" 
+import "./login.css"
 
 function Login() {
 
-    
 
 
-return (
-    <div className="backgroundTest">
-         <p>Commit inicial :)</p>    
-    </div>
+
+    return (
+        <div className="container">
+            <div className="headerLogin">  
+            <span>Logo</span>
+            <button className="butHome"><a href="#">home</a></button> 
+            </div>
+            <div className="loginBox">
+                <span>Login</span>
+                <form className="formLogin">
+                    <label>Email</label>
+                    <input className="itensLogin"/>
+                    <label>Senha</label>
+                    <input className="itensLogin" type="password"/>
+                    <button className="buttonLogin">Acessar</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
