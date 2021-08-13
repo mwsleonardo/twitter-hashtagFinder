2201ca67e6fe4ab6111291c48bd399c9c83f968c
mwsleonardo <mwsleonardo@gmail.com>
2021-08-12 22:48:35 -0300

    Ajuste no style do carousel e imagens do resultado
    Adicionado título da busca e imagens de teste para o resultado. Atualização no homepage.css e item.js para ficar no molde do layout

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,28 +1,43 @@
 import React from 'react';
+import './homepage.css';
+
+// Import do carousel e styled-components(css) das imagens 
 import Carousel from "react-elastic-carousel";
 import Item from './item.js';
 
-import './homepage.css';
+// Import das imagens e icones usados na homepage
 import logoWhite from "./imgs/logo-white.svg";
 import logoSobre from "./imgs/icon-info-circle.svg";
 import logoLogin from "./imgs/icon-user-alt.svg";
 
+// Import das imagens teste para o resultado da busca
+import search1 from "./imgsSearch/search1.png";
+import search2 from "./imgsSearch/search2.png";
+import search3 from "./imgsSearch/search3.png";
+import search4 from "./imgsSearch/search4.png";
+import search5 from "./imgsSearch/search5.png";
+import search6 from "./imgsSearch/search6.png";
+import search7 from "./imgsSearch/search7.png";
+import search8 from "./imgsSearch/search8.png";
+import search9 from "./imgsSearch/search9.png";
+import search10 from "./imgsSearch/search10.png";
 
+// Const para a "quebra de linha" do resultado das imagens
 const breakPoints = [
     { width: 1, itemsToShow: 1 },
     { width: 550, itemsToShow: 2 },
     { width: 768, itemsToShow: 3 },
-    { width: 1200, itemsToShow: 4 },
+    { width: 1200, itemsToShow: 5 },
   ];
 
 
-
+// Function retornando todo o conteúdo da homepage
 function Homepage() {
 
     return (
+
         <div>
             <div className="homepage">
-
                 <header className="banner">
                     <div className="title">
                         <img src={logoWhite} alt="Logo" class="logo"></img>
@@ -41,39 +56,32 @@ function Homepage() {
                         <h1 className="title"> Encontre hashtags de maneira fácil  </h1>
                         <p className="subtitle"> Digite o que deseja no campo de buscas e confira os resultados do Twitter abaixo </p>
                     </div>
-
                     <div className="inputDiv">
                         <input type="text" className="searchBar" placeholder="Buscar..."></input>
                     </div>
                 </header>
 
 
-                <h1 style={{ textAlign: "center" }}>Example to setup your carousel in react</h1>
-            <div className="carouselImages">
-            <Carousel breakPoints={breakPoints}>
-            <Item>One</Item>
-            <Item>Two</Item>
-            <Item>Three</Item>
-            <Item>Four</Item>
-            <Item>Five</Item>
-            <Item>Six</Item>
-            <Item>Seven</Item>
-            <Item>Eight</Item>
-            </Carousel>
-      </div>
-
-
-
-
-
-
-
+                <h1 className="searchTitle">Exibindo os 10 resultados mais recentes de #vikings</h1>
+                    <div className="carouselImages">
+                     <Carousel breakPoints={breakPoints}>
+                        <Item><img className="resultImage" src={search1}></img></Item>
+                        <Item><img className="resultImage" src={search2}></img></Item>
+                        <Item><img className="resultImage" src={search3}></img></Item>
+                        <Item><img className="resultImage" src={search4}></img></Item>
+                        <Item><img className="resultImage" src={search5}></img></Item>
+                        <Item><img className="resultImage" src={search6}></img></Item>
+                        <Item><img className="resultImage" src={search7}></img></Item>
+                        <Item><img className="resultImage" src={search8}></img></Item>
+                        <Item><img className="resultImage" src={search9}></img></Item>
+                        <Item><img className="resultImage" src={search10}></img></Item>
+                    </Carousel>
+                </div>
+                <div className="imageResult1">teste</div>
                 <footer className="footer">
                     @NewTab Academy 2021. Todos os direitos reservados
                 </footer>
-
             </div>
-
         </div>
 
 
--- src/components/item.js
+++ src/components/item.js
@@ -4,10 +4,13 @@ export default styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 250px;
-  width: 100%;
+  height: 287px;
+  width: 287px;
   background-color: #00008B;
   color: #fff;
-  margin: 0 15px;
+  margin: 0 15px; 
   font-size: 4em;
-`;
+  border: 0px solid black;
+  border-radius: 21px;
+  box-shadow: 0px 9px 27px #5538EE29;
+`;  
