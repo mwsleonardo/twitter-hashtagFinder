5315747e5ca667ad8e9352bb1e1f21d66e8ac344
mwsleonardo <mwsleonardo@gmail.com>
2021-08-18 22:21:04 -0300

    Atualização deploy gh-pages

--- src/index.jsx
+++ src/index.jsx
@@ -4,19 +4,19 @@ import Homepage from './components/homepage'
 import Login from './components/login'
 import About from './components/About'
 import Search from './components/search'
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {HashRouter, BrowserRouter, Switch, Route} from 'react-router-dom'
 
 
 // Utilizando react-router-dom para fazer as rotas entre as páginas 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route path="/" component={Homepage} exact/>
       <Route path="/login" component={Login} />
       <Route path="/About" component={About} />
       <Route path="/search" component={Search} />
     </Switch>
-  </BrowserRouter>,
+  </HashRouter>,
 
   document.getElementById('root')
 
