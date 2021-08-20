e7f68298082e32adc1cd3cb99f3c9a69f62d1f9d
Giovanni Shiroma <giovanni.shiroma04@gmail.com>
2021-08-20 12:13:22 -0300

    Validaçao do formulário

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import { useState } from 'react';
 import "./login.css"
 import loginBg from './imgs/login-bg.jpg'
 import logoWhite from './imgs/logo-white.svg'
 import iconHome from './imgs/icon-home.svg'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Login() {
+
+    const [campos, setCampos] = useState('');
+    const [campos2, setCampos2] = useState('');
+    const [error, setError] = useState(null);
+
+    function validateCampos (value) {
+        if (value.length === 0) {
+            setError('Preencha este campo.');
+            return false;
+        } else {
+            setError(null);
+            return true;
+        }
+
+    }
+
+    function validateCampos2 (value) {
+        if (value.length === 0) {
+            setError('Preencha este campo.');
+            return false;
+        } else {
+            setError(null);
+            return true;
+        }
+
+    }
+
+
+    function handleBlur({ target }) {
+        validateCampos(target.value);
+        validateCampos2(target.value);
+    }
+
+    function handleChange({ target }) {
+        if (error) validateCampos(target.value);
+
+        setCampos(target.value);
+        setCampos2(target.value);
+    }
 
     return (
         <div className="containerLogin">
@@ -14,13 +53,15 @@ function Login() {
                 <img src={logoWhite} className="itensHeader"></img>
                 <Link to="/"><button className="butHome itensHeader"><img src={iconHome}></img> <span>Home</span></button></Link>
             </div>
-            <div className="loginBox">
+            <div className="loginBox" >
                 <h1 className="titleBox">Login</h1>
                 <form className="formLogin">
-                    <input className="itensLogin" placeholder="Usuário"  />
-                    <input className="itensLogin" type="password" placeholder="Senha" />
+                    <input className="itensLogin" placeholder="Usuário" value={campos} onChange={handleChange} onBlur={handleBlur} />
+                    {error && <p className="invalidUser">{error}</p>}
+                    <input className="itensLogin" type="password" value={campos2} onChange={handleChange} onBlur={handleBlur} placeholder="Senha" />
+                    {error && <p className="invalidUser">{error}</p>}
                 </form>
-                <button className="buttonLogin">Acessar</button>
+                <button className="buttonLogin" onClick={ handleChange}> Acessar</button>
             </div>
         </div>
     )
