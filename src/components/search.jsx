a1293340edb2249450970f978a80ce79481faf21
JulianaGoia <julianakgoia@gmail.com>
2021-08-19 22:56:14 -0300

    adição da requisição da API

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 import './search.css';
 import homeIcon from './images_v01/icon-home.svg'
 import exitIcon from './images_v01/icon-power-off.svg'
 import {Link} from 'react-router-dom'
 import loginBg from './imgs/login-bg.jpg'
+import { useEffect, useState } from 'react';
 
 function Search() {
+
+    const [hashtagList, setHashtagList] = useState([]); //armazena todos as hashtags buscadas e armazenadas na API
+
+    //requisiçao para API - metodo GET | o useEffect evita que a funçao seja feita infinitamente
+    useEffect(() => {
+
+        //funçao será realizada após o componente for renderizado
+        axios.get("https://airtable.com/app6wQWfM6eJngkD4/api/docs#curl/table:buscas:list")
+
+        //se a requisiçao der certo, executa o .then
+        .then((response) => {
+            setHashtagList(response.data)
+        })
+
+    }, [])
  
     return (
         <>
         {/* navegador */}   
-        
         <div className="containerSearch">
         <img src={loginBg} className="backgroundImg"></img>
 
         <div>            
             <div class="header">
                 <p class="homeTitle">hashtag<strong>finder</strong></p>
+                
                 {/* botão HOME */} 
                 <Link to="/" className="linkRoute">
                 <button class="homeButton">
