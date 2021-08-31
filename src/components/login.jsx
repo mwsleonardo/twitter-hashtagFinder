725c982a41d9e1071ac8a587584fbf8ad710dcbb
giovanni Shiroma <giovanni.shiroma04@gmail.com>
2021-08-31 01:21:15 -0300

    requisição api e inicio validação de cadastro

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,86 +1,64 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import "./login.css"
 import loginBg from './imgs/login-bg.jpg'
 import logoWhite from './imgs/logo-white.svg'
 import iconHome from './imgs/icon-home.svg'
 import { Link } from 'react-router-dom'
-import { faExclamation } from '@fortawesome/free-solid-svg-icons';
+// import { faExclamation } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios'
+// import api from './apiLogin';
+// 
 
 
-    
 
 function Login() {
-    
-        // Const menssagem validação
-        const [error, setError] = useState(null);
-        const [error2, setError2] = useState(null);
-        // Inputs no form 
-        const [userInput, setUserInput] = useState('');
-        const [passwordInput, setPasswordInput] = useState('');
-    
-    function getInputs() {
-            axios.get("https://airtable.com/shrTHWqQB8tBDdE3i/tblVomDZ8sk5GVbKY"(), {
-                "records": [
-                    {
-                        "fields": {
-                            "Squad": "2",
-                            "Email": userInput,
-                            "Senha": passwordInput
-                        }
-                    }
-                ]
-            }, {
-                headers: {
-                    "Authorization": "Bearer key2CwkHb0CKumjuM",
-                    "Content-Type": "application/json"
-                }
-            });
-        }
 
-        function validateDados(){
-        const [email] = "contato.newtab@gmail.com"
-            
-        if (userInput.length === email){ // se o campo estiver vazio, impede que seja registrado
-            // Campo não preechido 
-            setError('email errado!'); // Aparece mensagem de erro
-            event.preventDefault();
-        } else if (passwordInput.length == 0){
-            // Campo não preechido 
-             setError('') 
-             setError2('Campo obrigatório!') // Aparece mensagem de erro 
-             event.preventDefault();
-        } else {
-            // Mensagem de erro é apagada 
-            setError(null); 
-            setError2(null);
-            console.log('CREDENCIAIS DE USUARIO =>','USER:' , userInput ,"SENHA:", passwordInput);
-            return true;
-        } 
-    }
+    // Const menssagem validação
+    const [error, setError] = useState(null);
+    const [error2, setError2] = useState(null);
+    // Inputs no form 
+    const [userInput, setUserInput] = useState('');
+    const [passwordInput, setPasswordInput] = useState('');
+
+    const[posts, setPosts ] = useState([])
+
+
+    useEffect(() => {
+        axios.get("https://api.airtable.com/v0/app6wQWfM6eJngkD4/Login?maxRecords=3&view=Grid%20view", {
+            headers: {
+                "Authorization": "Bearer key2CwkHb0CKumjuM"
+            }
+        }).then((response) => {
+            console.log(response.data)
+        })
+            .catch(() => {
+                console.log('deu errado')
+            })
+    })
 
 
     function submitForm(event) {
         // event.preventDefault(); // evita recarregamento da página
-        if (userInput.length == 0){ // se o campo estiver vazio, impede que seja registrado
+        if (userInput.length == 0) { // se o campo estiver vazio, impede que seja registrado
             // Campo não preechido 
             setError('Campo obrigatório!'); // Aparece mensagem de erro
             event.preventDefault();
             return false;
-        } else if (passwordInput.length == 0){
+        } else if (passwordInput.length == 0) {
             // Campo não preechido 
-             setError('') 
-             setError2('Campo obrigatório!') // Aparece mensagem de erro 
-             event.preventDefault();
+            setError('')
+            setError2('Campo obrigatório!') // Aparece mensagem de erro 
+            event.preventDefault();
         } else {
             // Mensagem de erro é apagada 
-            setError(null); 
+            setError(null);
             setError2(null);
-            console.log('CREDENCIAIS DE USUARIO =>','USER:' , userInput ,"SENHA:", passwordInput);
+            console.log('CREDENCIAIS DE USUARIO =>', 'USER:', userInput, "SENHA:", passwordInput);
             return true;
-        } 
+        }
     }
 
-    
+
 
     function handleTextChange(event) {
         setUserInput(event.target.value); // guarda o valor preenchido no content Input
@@ -88,9 +66,11 @@ function Login() {
 
     function handleTextChange2(event) {
         setPasswordInput(event.target.value); // guarda o valor preenchido no content Input
-    }   
+    }
 
-    
+
+
+
 
     return (
         <div className="containerLogin">
@@ -101,25 +81,24 @@ function Login() {
             </div>
             <div className="loginBox" >
                 <h1 className="titleBox">Login</h1>
-                <form className="formLogin">
+                <form className="formLogin" onSubmit={useEffect}>
                     <input
                         className="itensLogin"
                         placeholder="Usuário"
                         value={userInput} // valor do input 
                         onChange={handleTextChange}  // guarda o valor preenchido 
-                        />
-                        {error && <p className="invalidUser">{error}</p>}
+                    />
+                    {error && <p className="invalidUser">{error}</p>}
                     <input
                         className="itensLogin"
                         type="password"
-                        value=""
                         placeholder="Senha"
                         value={passwordInput} // valor do input 
                         onChange={handleTextChange2} // guarda o valor preenchido
-                        />
-                        {error2 && <p className="invalidUser">{error2}</p>}
+                    />
+                    {error2 && <p className="invalidUser">{error2}</p>}
                 </form>
-                <Link to="/search"><button className="buttonLogin" onClick={submitForm ,  getInputs ,  validateDados}> Acessar</button></Link>
+                <Link to="/search"><button className="buttonLogin" type="submit" onClick={submitForm}> Acessar</button></Link>
             </div>
         </div>
     )
@@ -127,3 +106,43 @@ function Login() {
 
 
 export default Login;
+
+// function getInputs() {
+//         axios.get("https://airtable.com/shrTHWqQB8tBDdE3i/tblVomDZ8sk5GVbKY"(), {
+//             "records": [
+//                 {
+//                     "fields": {
+//                         "Squad": "2",
+//                         "Email": userInput,
+//                         "Senha": passwordInput
+//                     }
+//                 }
+//             ]
+//         }, {
+//             headers: {
+//                 "Authorization": "Bearer key2CwkHb0CKumjuM",
+//                 "Content-Type": "application/json"
+//             }
+//         });
+//     }
+
+//     function validateDados(){
+//     const [email] = "contato.newtab@gmail.com"
+
+//     if (userInput.length === email){ // se o campo estiver vazio, impede que seja registrado
+//         // Campo não preechido 
+//         setError('email errado!'); // Aparece mensagem de erro
+//         event.preventDefault();
+//     } else if (passwordInput.length == 0){
+//         // Campo não preechido 
+//          setError('') 
+//          setError2('Campo obrigatório!') // Aparece mensagem de erro 
+//          event.preventDefault();
+//     } else {
+//         // Mensagem de erro é apagada 
+//         setError(null); 
+//         setError2(null);
+//         console.log('CREDENCIAIS DE USUARIO =>','USER:' , userInput ,"SENHA:", passwordInput);
+//         return true;
+//     } 
+// }
