943284359c477d854e18fa686d390e8b48a8f302
JulianaGoia <julianakgoia@gmail.com>
2021-08-30 22:21:32 -0300

    revisão do componente InfiniteScroll

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -6,47 +6,51 @@ import exitIcon from './images_v01/icon-power-off.svg'
 import {Link} from 'react-router-dom'
 import loginBg from './imgs/login-bg.jpg'
 import { useEffect, useState } from 'react';
-import InfiniteLoading from "react-simple-infinite-loading";
+// import InfiniteLoading from "react-simple-infinite-loading";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 
 function Search() {
     
-    //const [hashtagList, setHashtagList] = useState([]); // guarda as hashtags registradas na API
-    const [hashtagList, setHashtagList] = useState([...Array(100)].map((_, index) => index));
+    const [hashtagList, setHashtagList] = useState([]); // guarda as hashtags registradas na API
+    const [hasMore, setHasMore] = useState(true);
 
-    const loadMoreItems = () => {
-        const newList = [...Array(10)].map((_, index) => hashtagList.length + index);
+    const fetchData = () => {
+        const newList = [...Array(100)].map((_, index) => hashtagList.length + index);
     
         return new Promise(resolve => {
           setTimeout(() => {
             setHashtagList([...hashtagList, ...newList]);
+            setHasMore(false);
             resolve();
-          }, 30);
+          }, 100);
         });
       };
 
 
-useEffect(() => {
-    axios.get("https://api.airtable.com/v0/app6wQWfM6eJngkD4/tbl4mrtX1Owvos7eB?filterByFormula=%7BSquad%7D+%3D+'52'", {
-        headers: {
-            "Authorization": "Bearer key2CwkHb0CKumjuM"
-        }
-    }).then (response => {
-            const infos = response.data.records.map(
-                info => {
-                    return {
-                        "squad": '52',
-                        "hashtag": info.fields.Hashtag,
-                        "data": info.fields.Data,
-                        "hora": info.fields.Hora
+    useEffect(() => {
+        axios.get("https://api.airtable.com/v0/app6wQWfM6eJngkD4/tbl4mrtX1Owvos7eB?filterByFormula=%7BSquad%7D+%3D+'52'", {
+            headers: {
+                "Authorization": "Bearer key2CwkHb0CKumjuM"
+            }
+        }).then (response => {
+                const infos = response.data.records.map(
+                    info => {
+                        return {
+                            "squad": '52',
+                            "hashtag": info.fields.Hashtag,
+                            "data": info.fields.Data,
+                            "hora": info.fields.Hora
+                        }
                     }
-                }
-            )
-            setHashtagList(infos);
-                console.log(infos);
-        }
-    )
-}, []);
+                )
+
+                setHashtagList(infos);
+                    console.log(infos);
+            }
+        )
+    }, []);
 
 
     return (
@@ -81,7 +85,6 @@ useEffect(() => {
             <h1>Buscas realizadas</h1>
         </div>
 
-
         <div class="container"> 
             <thead>
                 <tr>
@@ -90,15 +93,21 @@ useEffect(() => {
                     <th>Hora</th>
                 </tr>
             </thead>
-
-            <InfiniteLoading
-                hasMoreItems
-                itemHeight = {40}
-                loadMoreItems = {loadMoreItems}
+            
+            {/* Componente de InfiniteScroll */} 
+            <InfiniteScroll
+              dataLength={hashtagList.length} //render the next data
+              next={fetchData}
+              hasMore={hasMore}
+              loader={<h4>Loading...</h4>}
+              endMessage={
+                <p style={{ textAlign: 'center' }}>
+                  <b>Yay! You have seen it all</b>
+                </p>
+              }
             >
-
                 {/* mapeamento dos elementos da array, armazenados no state hashtagList */} 
-            {hashtagList.map ((obj, i) => {
+                {hashtagList.map ((obj, i) => {
                 return (
                     <table>
                         <tbody>
@@ -111,9 +120,11 @@ useEffect(() => {
                     </table>
                 )
             })}
-            </InfiniteLoading>
- 
+              
+            </InfiniteScroll>
+
         </div> 
+    
     </div>
 </div>
 </>
