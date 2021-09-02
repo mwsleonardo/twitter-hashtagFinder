eab5a0e2667531056c2de368d7f045833467f6a2
giovanni Shiroma <giovanni.shiroma04@gmail.com>
2021-09-01 23:46:15 -0300

    finalização

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,12 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import "./login.css"
 import loginBg from './imgs/login-bg.jpg'
 import logoWhite from './imgs/logo-white.svg'
 import iconHome from './imgs/icon-home.svg'
 import { Link } from 'react-router-dom'
-// import { faExclamation } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios'
-// import api from './apiLogin'; 
 
 function Login() {
 
@@ -19,29 +17,6 @@ function Login() {
 
     let [modalShow, setShowModal] = useState(false);
 
-    // function validateUser(e) {
-    //     const loginUser = [
-    //         { id: "recXC8gVKwJdnU2yr", 
-    //         Squad: "2", 
-    //         Senha: "123456", 
-    //         Email: "contato@newtab.academy" }
-    //     ];
-
-    //     if (userInput === loginUser.Email) { // se o campo estiver vazio, impede que seja registrado
-    //         alert('erro de usuario')
-    //         e.preventDefault()
-    //         return false;
-    //     } else if (passwordInput === loginUser.Senha) {
-    //         alert('erro de senha')
-    //         e.preventDefault();
-    //         return false;
-    //     } else {
-    //         alert("Usuario cadastrado");
-    //         return true;
-    //     }
-    // }
-
-    
     function submitForm(event) {
         // event.preventDefault(); // evita recarregamento da página
         if (userInput.length == 0) { // se o campo estiver vazio, impede que seja registrado
@@ -61,26 +36,25 @@ function Login() {
             setError2(null);
             console.log('CREDENCIAIS DE USUARIO =>', 'USER:', userInput, "SENHA:", passwordInput);
             // console.log('passou')
-            axios.get('https://api.airtable.com/v0/app6wQWfM6eJngkD4/Login?maxRecords=3&view=Grid%20view&filterByFormula=(AND({Email}=%22'+userInput+'%22,{Senha}=%22'+passwordInput+'%22))', {
+            axios.get('https://api.airtable.com/v0/app6wQWfM6eJngkD4/Login?maxRecords=3&view=Grid%20view&filterByFormula=(AND({Email}=%22' + userInput + '%22,{Senha}=%22' + passwordInput + '%22))', {
                 headers: {
                     "Authorization": "Bearer key2CwkHb0CKumjuM"
                 }
             }).then((response) => {
-                if (response.data.records.length == 0 ){
-                    alert('deu errado')
+                if (response.data.records.length == 0) {
+                    alert('Email e/ou Senha inválidos')
                     event.preventDefault();
                     return false;
                 } else {
                     console.log(response.data.records.length)
-                    alert('CADASTRO FUNFOU!')
                     setShowModal(true)
-                    return   true;
-                    
+                    return true;
+
                 }
             })
-            .catch(() => {
-                console.log('deu errado')
-            })
+                .catch(() => {
+                    console.log('deu errado')
+                })
         }
     }
 
@@ -95,7 +69,7 @@ function Login() {
 
     const handler = (event) => {
         if (event.key === 'Enter') {
-        submitForm();
+            submitForm();
         }
     }
 
@@ -125,66 +99,19 @@ function Login() {
                     />
                     {error2 && <p className="invalidUser">{error2}</p>}
                 </form>
-               <button className="buttonLogin" type="submit" onClick={submitForm}> Acessar</button>
+                <button className="buttonLogin" type="submit" onClick={submitForm}> Acessar </button>
             </div>
 
             <div className="backdrop" style={{ display: (modalShow ? 'block' : 'none') }} onSubmit={() => setShowModal(false)}></div>
 
             <div className="modalContainer" style={{ display: (modalShow ? 'block' : 'none') }}>
-                <div className="modalContent"> Credenciais validas
-                    <Link to="/search"><button className="buttonLogin" type="submit">Continuar</button></Link>
+                <div className="modalContent"> <h1>Cadastro concluído</h1>
+                    <Link to="/search"><button className="buttonModal" type="submit">Continuar</button></Link>
                 </div>
             </div>
-
-
-
-
-
-
-
         </div>
     )
 }
 
 
 export default Login;
-
-// function getInputs() {
-//         axios.get("https://airtable.com/shrTHWqQB8tBDdE3i/tblVomDZ8sk5GVbKY"(), {
-//             "records": [
-//                 {
-//                     "fields": {
-//                         "Squad": "2",
-//                         "Email": userInput,
-//                         "Senha": passwordInput
-//                     }
-//                 }
-//             ]
-//         }, {
-//             headers: {
-//                 "Authorization": "Bearer key2CwkHb0CKumjuM",
-//                 "Content-Type": "application/json"
-//             }
-//         });
-//     }
-
-//     function validateDados(){
-//     const [email] = "contato.newtab@gmail.com"
-
-//     if (userInput.length === email){ // se o campo estiver vazio, impede que seja registrado
-//         // Campo não preechido 
-//         setError('email errado!'); // Aparece mensagem de erro
-//         event.preventDefault();
-//     } else if (passwordInput.length == 0){
-//         // Campo não preechido 
-//          setError('') 
-//          setError2('Campo obrigatório!') // Aparece mensagem de erro 
-//          event.preventDefault();
-//     } else {
-//         // Mensagem de erro é apagada 
-//         setError(null); 
-//         setError2(null);
-//         console.log('CREDENCIAIS DE USUARIO =>','USER:' , userInput ,"SENHA:", passwordInput);
-//         return true;
-//     } 
-// }
